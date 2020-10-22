715dcdc0737947a98a17b80eff85eb4a09b9b631
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-10-22 01:51:33 +0000

    cut junit test added

--- src/test/java/uk/ac/ucl/jsh/JshTest.java
+++ src/test/java/uk/ac/ucl/jsh/JshTest.java
@@ -21,4 +21,18 @@ public class JshTest {
         assertEquals(scn.next(),"foo");
     }
 
+    @Test
+    public void test_cut() throws Exception {
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out;
+        out = new PipedOutputStream(in);
+        String[] cases = {"cut -b 1 text1.txt", "cut -b 1,2 text1.txt", "cut -b 1- text1.txt", "cut -b 1,3-4 text1.txt"};
+        String[] expected_out = {"a", "ab", "abcdef", "acd"};
+        for (int i = 0; i < cases.length; i++) {
+            Jsh.eval(cases[i], out);
+            Scanner scn = new Scanner(in);
+            assertEquals(scn.next(), expected_out[i]);
+        }
+    }
+
 }
