5cbaa542aeae43d7aa3ec2ba22a16f34cb3236a7
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-12-29 16:08:19 +0300

    tail done for IO redir

--- src/main/java/uk/ac/ucl/jsh/Applications/Tail.java
+++ src/main/java/uk/ac/ucl/jsh/Applications/Tail.java
@@ -6,12 +6,20 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tail implements Application {
+
+    private OutputStreamWriter writer;
 
     @Override
     public String mainExec(ArrayList<String> args, String currentDirectory, InputStream input, OutputStream output) throws IOException {
         String message = argCheck(args);
+
+        if (input != null && args.size() == 1) {
+            message = "nothing";
+        }
+
         if (!message.equals("nothing")) {
             throwError(message, output);
         } else {
@@ -26,37 +34,49 @@ public class Tail implements Application {
 
     @Override
     public String exec(ArrayList<String> args, String currentDirectory, InputStream input, OutputStream output) throws IOException {
-        OutputStreamWriter writer = new OutputStreamWriter(output);
+        writer = new OutputStreamWriter(output);
         int tailLines = 10;
         String tailArg;
 
-        if (args.size() == 4) {
-            try {
-                tailLines = Integer.parseInt(args.get(2));
-            } catch (Exception e) {
-                return "ERROR tail: wrong argument " + args.get(2);
-            }
-            tailArg = args.get(3);
+        if (args.size() == 1) {
+            writeOut(new Scanner(input));
         } else {
-            tailArg = args.get(1);
-        }
-
-        File tailFile = new File(currentDirectory + File.separator + tailArg);
-        if (tailFile.exists()) {
-            ArrayList<String> storage = new ArrayList<>(Files.readAllLines(Paths.get(String.valueOf(tailFile))));
-            try {
-                int index = ((tailLines <= storage.size()) ? storage.size() - tailLines : 0);
-                for (int i = index; i < storage.size(); i++) {
-                    writer.write(storage.get(i) + Jsh.lineSeparator);
-                    writer.flush();
+            if (args.size() == 4) {
+                try {
+                    tailLines = Integer.parseInt(args.get(2));
+                } catch (Exception e) {
+                    return "ERROR tail: wrong argument " + args.get(2);
                 }
-            } catch (IOException e) {
-                return "ERROR tail: cannot open " + tailArg;
+                tailArg = args.get(3);
+            } else {
+                tailArg = args.get(1);
             }
-        } else {
-            return "tail: " + tailArg + " does not exist";
+
+            File tailFile = new File(currentDirectory + File.separator + tailArg);
+            if (tailFile.exists()) {
+                ArrayList<String> storage = new ArrayList<>(Files.readAllLines(Paths.get(String.valueOf(tailFile))));
+                try {
+                    int index = ((tailLines <= storage.size()) ? storage.size() - tailLines : 0);
+                    for (int i = index; i < storage.size(); i++) {
+                        writer.write(storage.get(i) + Jsh.lineSeparator);
+                        writer.flush();
+                    }
+                } catch (IOException e) {
+                    return "ERROR tail: cannot open " + tailArg;
+                }
+            } else {
+                return "tail: " + tailArg + " does not exist";
+            }
         }
         return currentDirectory;
+    }
+
+    /* Prints to specified output */
+    private void writeOut(Scanner scn) throws IOException {
+        while (scn.hasNextLine()) {
+            writer.write(scn.nextLine() + Jsh.lineSeparator);
+            writer.flush();
+        }
     }
 
     /* Validates arguments input */
