de24660b5e3db3af60be8a11c1a2fbe5aedecb7d
Aashvin <55795472+Aashvin@users.noreply.github.com>
2021-01-01 10:34:08 +0000

    refactored find

--- src/main/java/uk/ac/ucl/jsh/Applications/Find.java
+++ src/main/java/uk/ac/ucl/jsh/Applications/Find.java
@@ -56,52 +56,66 @@ public class Find implements Application {
             for (File file : listOfFiles) {
                 if (!file.getName().startsWith(".")) {
                     HashMap<String, String> all_files = walkFileDirs(file);
-
-                    for (Map.Entry<String, String> entry : all_files.entrySet()) {
-                        if (!globbing) {
-                            for (String currentArgument : args.subList(argSizeCheck - 1, args.size())) {
-                                if (currentArgument.equals(entry.getValue())) {
-                                    if (entry.getKey().equals(System.lineSeparator() + entry.getValue())) {
-                                        result_set.add(entry.getValue());
-                                    } else {
-                                        if (!directorySpecified) {
-                                            result_set.add("." + entry.getKey());
-                                        } else {
-                                            result_set.add(entry.getKey().substring(1));
-                                        }
-                                    }
-                                }
-                            }
-                        } else {
-                            //System.out.println(args.get(args.size() - 1).substring(2));
-                            //System.out.print(entry.getValue().substring(entry.getValue().length() - args.get(args.size() - 1).substring(2).length()));
-                            if (args.get(args.size() - 1).substring(2).equals(entry.getValue().substring(entry.getValue().length() - args.get(args.size() - 1).substring(2).length()))) {
-                                if (entry.getKey().equals(System.lineSeparator() + entry.getValue())) {
-                                    if (!directorySpecified) {
-                                        result_set.add( "." + entry.getKey());
-                                    } else {
-                                        result_set.add(entry.getValue());
-                                    }
-                                } else {
-                                    if (!directorySpecified) {
-                                        result_set.add("." + entry.getKey());
-                                    } else {
-                                        result_set.add(entry.getKey().substring(1));
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    result_set.addAll(getCorrectFiles(args, all_files, globbing, directorySpecified, argSizeCheck));
                 }
             }
             writeOut(result_set, writer);
         } catch (NullPointerException e) {
             return "ERROR find: no such directory";
-            //throw new RuntimeException("find: no such directory");
         }
         return currentDirectory;
     }
 
+    /* Calls the relevant method to find the correct files in the list of all files */
+    private Set<String> getCorrectFiles(ArrayList<String> args, HashMap<String, String> all_files, Boolean globbing, Boolean directorySpecified, int argSizeCheck){
+        Set<String> result_set = new HashSet<>();
+        for (Map.Entry<String, String> entry : all_files.entrySet()) {
+            if (!globbing) {
+                result_set.addAll(caseSingleName(args, entry, directorySpecified, argSizeCheck));
+            } else {
+                result_set.addAll(caseGlobbing(args, entry, directorySpecified));
+            }
+        }
+        return result_set;
+    }
+
+    /* Finds the files with a matching name */
+    private Set<String> caseSingleName(ArrayList<String> args, Map.Entry<String, String> entry, Boolean directorySpecified, int argSizeCheck) {
+        Set<String> result_set = new HashSet<>();
+        for (String currentArgument : args.subList(argSizeCheck - 1, args.size())) {
+            if (currentArgument.equals(entry.getValue())) {
+                if (entry.getKey().equals(System.lineSeparator() + entry.getValue())) {
+                    result_set.add(entry.getValue());
+                } else {
+                    if (!directorySpecified) {
+                        result_set.add("." + entry.getKey());
+                    } else {
+                        result_set.add(entry.getKey().substring(1));
+                    }
+                }
+            }
+        }
+        return result_set;
+    }
+
+    /* Finds all the files with the file extension */
+    private Set<String> caseGlobbing(ArrayList<String> args, Map.Entry<String, String> entry, Boolean directorySpecified){
+        Set<String> result_set = new HashSet<>();
+        if (args.get(args.size() - 1).substring(2).equals(entry.getValue().substring(entry.getValue().length() - args.get(args.size() - 1).substring(2).length()))) {
+            if (!directorySpecified) {
+                result_set.add( "." + entry.getKey());
+            } else {
+                if (entry.getKey().equals(System.lineSeparator() + entry.getValue())) {
+                    result_set.add(entry.getValue());
+                } else {
+                    result_set.add(entry.getKey().substring(1));
+                }
+            }
+        }
+        return result_set;
+    }
+
+    /* Gets all files in a directory an it's subdirectories */
     private HashMap<String, String> walkFileDirs(File fileDirectory) throws IOException {
         HashMap<String, String> walk_result = new HashMap<>();
 
