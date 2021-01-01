e8ddc0f16374449deb4fbea2818b68691e1d309d
Aashvin <55795472+Aashvin@users.noreply.github.com>
2021-01-01 11:55:03 +0000

    refactored and commented tail

--- src/main/java/uk/ac/ucl/jsh/Applications/Tail.java
+++ src/main/java/uk/ac/ucl/jsh/Applications/Tail.java
@@ -37,58 +37,24 @@ public class Tail implements Application {
         writer = new OutputStreamWriter(output);
         ArrayList<String> new_args = new ArrayList<>();
         int tailLines = 10;
-        String tailArg;
+        String endResult;
 
         if (args.size() == 1) {
             Scanner scn = new Scanner(input);
             while (scn.hasNextLine()) {
                 new_args.add(scn.nextLine());
             }
-            returnTailLines(tailLines, new_args);
+            writeOut(tailLines, new_args);
         } else {
-            if (args.size() == 4) {
-                try {
-                    tailLines = Integer.parseInt(args.get(2));
-                } catch (Exception e) {
-                    return "ERROR tail: wrong argument " + args.get(2);
-                }
-                tailArg = args.get(3);
-            } else {
-                tailArg = args.get(1);
-            }
-
-            File tailFile = new File(currentDirectory + File.separator + tailArg);
-            if (tailFile.exists()) {
-                ArrayList<String> storage = new ArrayList<>(Files.readAllLines(Paths.get(String.valueOf(tailFile))));
-                try {
-                    returnTailLines(tailLines, storage);
-                } catch (IOException e) {
-                    return "ERROR tail: cannot open " + tailArg;
-                }
-            } else {
-                return "tail: " + tailArg + " does not exist";
+            endResult = normalInput(args, tailLines, currentDirectory);
+            if (!endResult.equals("SUCCESS")) {
+                return endResult;
             }
         }
         return currentDirectory;
     }
 
-    private void returnTailLines(int tailLines, ArrayList<String> storage) throws IOException {
-        int index = ((tailLines <= storage.size()) ? storage.size() - tailLines : 0);
-        for (int i = index; i < storage.size(); i++) {
-            writer.write(storage.get(i) + Jsh.lineSeparator);
-            writer.flush();
-        }
-    }
 
-    /* Prints to specified output */
-    private void writeOut(Scanner scn) throws IOException {
-        while (scn.hasNextLine()) {
-            writer.write(scn.nextLine() + Jsh.lineSeparator);
-            writer.flush();
-        }
-    }
-
-    /* Validates arguments input */
     @Override
     public String argCheck(ArrayList<String> args) {
         if (args.isEmpty()) {
@@ -105,5 +71,72 @@ public class Tail implements Application {
     @Override
     public void throwError(String message, OutputStream output) {
         throw new RuntimeException(message);
+    }
+
+    /**
+     * Function to print the correct number of lines from an ArrayList
+     * @return - void
+     */
+    private void writeOut(int tailLines, ArrayList<String> storage) throws IOException {
+        int index = ((tailLines <= storage.size()) ? storage.size() - tailLines : 0);
+        for (int i = index; i < storage.size(); i++) {
+            writer.write(storage.get(i) + Jsh.lineSeparator);
+            writer.flush();
+        }
+    }
+
+    /**
+     * Handles the case for a normal input: calls the methods to get the correct lines
+     * @return - String
+     */
+    private String normalInput(ArrayList<String> args, int tailLines, String currentDirectory) throws IOException {
+        String tailArg;
+        if (args.size() == 4) {
+            tailLines = changeTailLines(args);
+            tailArg = args.get(3);
+        } else {
+            tailArg = args.get(1);
+        }
+
+        if (tailLines == -1) return "ERROR tail: wrong argument " + args.get(2);
+
+        String correctLines = getCorrectLines(tailLines, tailArg, currentDirectory);
+        if (!correctLines.equals("SUCCESS")) {
+            return correctLines;
+        }
+        return "SUCCESS";
+    }
+
+    /**
+     * Checks if there is a number of lines specified
+     * @return - int
+     */
+    private int changeTailLines(ArrayList<String> args) {
+        int tailLines;
+        try {
+            tailLines = Integer.parseInt(args.get(2));
+        } catch (Exception e) {
+            tailLines = -1;
+        }
+        return tailLines;
+    }
+
+    /**
+     * Gets the correct number of lines and calls the WriteOut method with relevant parameters
+     * @return - String
+     */
+    private String getCorrectLines(int tailLines, String tailArg, String currentDirectory) throws IOException {
+        File tailFile = new File(currentDirectory + File.separator + tailArg);
+        if (tailFile.exists()) {
+            ArrayList<String> storage = new ArrayList<>(Files.readAllLines(Paths.get(String.valueOf(tailFile))));
+            try {
+                writeOut(tailLines, storage);
+            } catch (IOException e) {
+                return "ERROR tail: cannot open " + tailArg;
+            }
+        } else {
+            return "ERROR tail: " + tailArg + " does not exist";
+        }
+        return "SUCCESS";
     }
 }
