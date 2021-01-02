eccb71034b6c72f0269ba782a510eb9acab30d45
Aashvin <55795472+Aashvin@users.noreply.github.com>
2021-01-02 11:12:46 +0000

    finished head tests

--- src/test/java/uk/ac/ucl/jsh/AppTests/HeadTest.java
+++ src/test/java/uk/ac/ucl/jsh/AppTests/HeadTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import uk.ac.ucl.jsh.Jsh;
 import uk.ac.ucl.jsh.JshTest;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
@@ -14,32 +15,50 @@ public class HeadTest extends JshTest {
     }
 
     @Test
-    public void test_1() throws IOException {
-        String[] args = {"head " + JshTest.testDirectory + "text1.txt", readFile(JshTest.testDirectory + "text1.txt")};
+    public void test_head_simple() throws IOException {
+        String[] args = {"head " + testDirectory + File.separator + "text1.txt", readFile(testDirectory + File.separator + "text1.txt")};
         Jsh.eval(args[0], this.out);
         String result = getEvalResult(args[1]);
         assertEquals(args[1], result);
     }
 
     @Test
-    public void test_2() {
-        String[] args = {"head -n 7 " + JshTest.testDirectory + "text3.txt", "AAA\nBBB\nAAA\nCCC\nccc\na\nb"};
+    public void test_head_7() {
+        String[] args = {"head -n 7 " + testDirectory + File.separator + "text3.txt", "AAA\nBBB\nAAA\nCCC\nccc\na\nb"};
         Jsh.eval(args[0], this.out);
         String result = getEvalResult(args[1]);
         assertEquals(args[1], result);
     }
 
     @Test
-    public void test_3() {
-        String[] args = {"head -n 0 " + JshTest.testDirectory + "text1.txt", ""};
+    public void test_head_0() {
+        String[] args = {"head -n 0 " + testDirectory + File.separator + "text1.txt", ""};
+        Jsh.eval(args[0], this.out);
+        String result = getEvalResult(args[1]);
+        assertEquals(args[1], result);
+    }
+
+    @Test
+    public void test_head_subdir() {
+        String[] args = {"head " + subDirString + File.separator + "text1.txt", "1\n2\n3\n4\n5"};
+        Jsh.eval(args[0], this.out);
+        String result = getEvalResult(args[1]);
+        assertEquals(args[1], result);
+    }
+
+    @Test
+    public void test_head_subdir_2() {
+        String[] args = {"head -n 2 " + subDirString + File.separator + "text2.txt", "A\na"};
         Jsh.eval(args[0], this.out);
         String result = getEvalResult(args[1]);
         assertEquals(args[1], result);
     }
 
     public void runAllTests() throws IOException {
-        test_1();
-        test_2();
-        test_3();
+        test_head_simple();
+        test_head_7();
+        test_head_0();
+        test_head_subdir();
+        test_head_subdir_2();
     }
 }
