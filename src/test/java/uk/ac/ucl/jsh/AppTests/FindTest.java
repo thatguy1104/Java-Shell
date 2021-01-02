12515f87ed58d9f6a7077aefa42261349cd55c69
Aashvin <55795472+Aashvin@users.noreply.github.com>
2021-01-02 12:34:10 +0000

    fixed find test

--- src/test/java/uk/ac/ucl/jsh/AppTests/FindTest.java
+++ src/test/java/uk/ac/ucl/jsh/AppTests/FindTest.java
@@ -7,6 +7,10 @@ import uk.ac.ucl.jsh.JshTest;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -18,51 +22,106 @@ public class FindTest extends JshTest {
 
     @Test
     public void test_find() {
-        String[] cases = {"cd testDir; find -name text1.txt; cd ..", "." + File.separator + "text1.txt\n" +
-                                                                     "." + File.separator + testSubDirectory + File.separator + "text1.txt"};
-        Jsh.eval(cases[0], out);
-        String result = getEvalResult(cases[1]);
-        assertEquals(cases[1], result);
+        String testCase = "cd testDir; find -name text1.txt; cd ..";
+        Jsh.eval(testCase, out);
+        String evalCase = "." + File.separator + "text1.txt\n" +
+                          "." + File.separator + testSubDirectory + File.separator + "text1.txt";
+        String expectedArray[] = evalCase.split("\n");
+        Set<String> expectedSet = new HashSet<>();
+        for (String expected : expectedArray) {
+            expectedSet.add(expected);
+        }
+        String result = getEvalResult(evalCase);
+        String resultArray[] = result.split("\n");
+        Set<String> resultSet = new HashSet<>();
+        for (String results : resultArray) {
+            resultSet.add(results);
+        }
+        assertEquals(expectedSet, resultSet);
     }
 
     @Test
     public void test_find_dir() {
-        String[] cases = {"find testDir -name text1.txt", testDirectory + File.separator + testSubDirectory + File.separator + "text1.txt\n" +
-                                                          testDirectory + File.separator + "text1.txt"};
-        Jsh.eval(cases[0], out);
-        String result = getEvalResult(cases[1]);
-        assertEquals(cases[1], result);
+        String testCase = "find testDir -name text1.txt";
+        Jsh.eval(testCase, out);
+        String evalCase = testDirectory + File.separator + testSubDirectory + File.separator + "text1.txt\n" +
+                          testDirectory + File.separator + "text1.txt";
+        String expectedArray[] = evalCase.split("\n");
+        Set<String> expectedSet = new HashSet<>();
+        for (String expected : expectedArray) {
+            expectedSet.add(expected);
+        }
+        String result = getEvalResult(evalCase);
+        String resultArray[] = result.split("\n");
+        Set<String> resultSet = new HashSet<>();
+        for (String results : resultArray) {
+            resultSet.add(results);
+        }
+        assertEquals(expectedSet, resultSet);
     }
 
     @Test
     public void test_find_glob() {
-        String[] cases = {"cd testDir; find -name *.txt; cd ..", "." + File.separator + testSubDirectory + File.separator + "text3.txt\n" +
-                                                                 "." + File.separator + "text2.txt\n" +
-                                                                 "." + File.separator + "text3.txt\n" +
-                                                                 "." + File.separator + "text1.txt\n" +
-                                                                 "." + File.separator + testSubDirectory + File.separator + "text2.txt\n" +
-                                                                 "." + File.separator + testSubDirectory + File.separator + "text1.txt"};
-        Jsh.eval(cases[0], out);
-        String result = getEvalResult(cases[1]);
-        assertEquals(cases[1], result);
+        String testCase = "cd testDir; find -name *.txt; cd ..";
+        Jsh.eval(testCase, out);
+        String evalCase = "." + File.separator + testSubDirectory + File.separator + "text3.txt\n" +
+                          "." + File.separator + "text2.txt\n" +
+                          "." + File.separator + "text3.txt\n" +
+                          "." + File.separator + "text1.txt\n" +
+                          "." + File.separator + testSubDirectory + File.separator + "text2.txt\n" +
+                          "." + File.separator + testSubDirectory + File.separator + "text1.txt";
+        String expectedArray[] = evalCase.split("\n");
+        Set<String> expectedSet = new HashSet<>();
+        for (String expected : expectedArray) {
+            expectedSet.add(expected);
+        }
+        String result = getEvalResult(evalCase);
+        String resultArray[] = result.split("\n");
+        Set<String> resultSet = new HashSet<>();
+        for (String results : resultArray) {
+            resultSet.add(results);
+        }
+        assertEquals(expectedSet, resultSet);
     }
 
     @Test
     public void test_find_dir_glob() {
-        String[] cases = {"find testDir/testSubDir -name *.txt", testDirectory + File.separator + testSubDirectory + File.separator + "text2.txt\n" +
-                                                                 testDirectory + File.separator + testSubDirectory + File.separator + "text3.txt\n" +
-                                                                 testDirectory + File.separator + testSubDirectory + File.separator + "text1.txt"};
-        Jsh.eval(cases[0], out);
-        String result = getEvalResult(cases[1]);
-        assertEquals(cases[1], result);
+        String testCase = "find testDir/testSubDir -name *.txt";
+        Jsh.eval(testCase, out);
+        String evalCase = testDirectory + File.separator + testSubDirectory + File.separator + "text2.txt\n" +
+                          testDirectory + File.separator + testSubDirectory + File.separator + "text3.txt\n" +
+                          testDirectory + File.separator + testSubDirectory + File.separator + "text1.txt";
+        String expectedArray[] = evalCase.split("\n");
+        Set<String> expectedSet = new HashSet<>();
+        for (String expected : expectedArray) {
+            expectedSet.add(expected);
+        }
+        String result = getEvalResult(evalCase);
+        String resultArray[] = result.split("\n");
+        Set<String> resultSet = new HashSet<>();
+        for (String results : resultArray) {
+            resultSet.add(results);
+        }
+        assertEquals(expectedSet, resultSet);
     }
 
     @Test
     public void test_find_subDir() {
-        String[] cases = {"cd testDir/testSubDir; find -name text1.txt; cd ..; cd ..", "." + File.separator + "text1.txt"};
-        Jsh.eval(cases[0], out);
-        String result = getEvalResult(cases[1]);
-        assertEquals(cases[1], result);
+        String testCase = "cd testDir/testSubDir; find -name text1.txt; cd ..; cd ..";
+        Jsh.eval(testCase, out);
+        String evalCase = "." + File.separator + "text1.txt";
+        String expectedArray[] = evalCase.split("\n");
+        Set<String> expectedSet = new HashSet<>();
+        for (String expected : expectedArray) {
+            expectedSet.add(expected);
+        }
+        String result = getEvalResult(evalCase);
+        String resultArray[] = result.split("\n");
+        Set<String> resultSet = new HashSet<>();
+        for (String results : resultArray) {
+            resultSet.add(results);
+        }
+        assertEquals(expectedSet, resultSet);
     }
 
     public void runAllTests() {
