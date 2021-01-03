e4d29e1300dfa5d7c7dc41ca2973de79e30e2c78
DanJav <daniel.javadinejad.19@ucl.ac.uk>
2021-01-04 00:43:26 +0100

    Head 90% branch coverage

--- src/test/java/uk/ac/ucl/jsh/AppTests/HeadTest.java
+++ src/test/java/uk/ac/ucl/jsh/AppTests/HeadTest.java
@@ -1,11 +1,16 @@
 package uk.ac.ucl.jsh.AppTests;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import uk.ac.ucl.jsh.Applications.Head;
 import uk.ac.ucl.jsh.Jsh;
 import uk.ac.ucl.jsh.JshTest;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -53,4 +58,58 @@ public class HeadTest extends JshTest {
         String result = getEvalResult(args[1]);
         assertEquals(args[1], result);
     }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void test_head_four_args_without_n() throws IOException{
+        Head head = new Head();
+        ArrayList<String> args = new ArrayList<>();
+        args.add("head");args.add("a");args.add("b");args.add("c");
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("head: wrong argument a");
+        head.mainExec(args, System.getProperty("user.dir"), InputStream.nullInputStream(), out);
+    }
+
+    @Test
+    public void test_head_three_args() throws IOException{
+        Head head = new Head();
+        ArrayList<String> args = new ArrayList<>();
+        args.add("head");args.add("a");args.add("b");
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("head: wrong arguments");
+        head.mainExec(args, System.getProperty("user.dir"), InputStream.nullInputStream(), out);
+    }
+
+    @Test
+    public void test_head_five_args() throws IOException{
+        Head head = new Head();
+        ArrayList<String> args = new ArrayList<>();
+        args.add("head");args.add("a");args.add("b");args.add("c");args.add("d");
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("head: wrong arguments");
+        head.mainExec(args, System.getProperty("user.dir"), InputStream.nullInputStream(), out);
+    }
+
+    //@Test TODO
+    public void test_head_nonexistentFile() throws IOException{
+        Head head = new Head();
+        ArrayList<String> args = new ArrayList<>();
+        args.add("head");args.add("nonexistentFile");
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("head: nonexistentFile does not exist");
+        head.mainExec(args, System.getProperty("user.dir"), InputStream.nullInputStream(), out);
+    }
+
+    @Test
+    public void test_head_missing_head_count() throws IOException{
+        Head head = new Head();
+        ArrayList<String> args = new ArrayList<>();
+        args.add("head");args.add("-n");args.add("a");args.add("b");
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("head: wrong argument a");
+        head.mainExec(args, System.getProperty("user.dir"), InputStream.nullInputStream(), out);
+    }
+
 }
